d538aafbbf153f7aaa25e5a20227c0eea1ede3f6
Edmund Nillas III <munilla333d@gmail.com>
2022-10-03 16:43:39 +0800

    Remove footer component

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div>
-      <p>Cor Jesu College</p>
-    </div>
-  );
-};
-
-export default Footer;
--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,3 @@
-import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
@@ -6,7 +5,6 @@ export default function Layout({ children }) {
     <>
       <Navbar />
       <main>{children}</main>
-      <Footer />
     </>
   );
 }
