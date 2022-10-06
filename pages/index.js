932f623323fe537a56f154379f55eb3fd8a19efa
Edmund Nillas III <munilla333d@gmail.com>
2022-10-06 14:04:53 +0800

    Style first content box

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ export default function Home() {
   return (
     <div>
       <Background>
-        <div className="container">
-          <h3>College of Computer and Information Science</h3>
-          <p>Home Page</p>
-          <hr className="solid" />
+        <div className="flex-container container">
+          <div className="flex-container title-container" role="seperator">
+            <h3>College of Computer and Information Science</h3>
+            <p>Home Page</p>
+          </div>
+          <div className="vl"></div>
+          <div className="flex-container title-container">
+            <p>Ambot lang unsay akong ibutang diri</p>
+          </div>
         </div>
       </Background>
     </div>
