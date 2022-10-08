7982fe87ad1f30a8415e196bc9bc41b47136ace8
Edmund Nillas III <munilla333d@gmail.com>
2022-10-09 06:35:49 +0800

    Refactor title to persist across different pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import Navbar from "./Navbar";
@@ -6,6 +7,28 @@ import logo from "../public/images/ccis-logo.png";
 export default function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <title>CCIS CJC</title>
+        <meta property="og:title" content="CCIS CJC" key="title" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+      </Head>
       <div className="flex-container header">
         <Link href="/">
           <a>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Announcements from "../components/Announcements";
 import Background from "../components/Background";
 import Calendar from "../components/Calendar";
@@ -6,28 +5,6 @@ import Calendar from "../components/Calendar";
 export default function Home() {
   return (
     <div className="home">
-      <Head>
-        <title>CCIS CJC</title>
-        <meta property="og:title" content="CCIS CJC" key="title" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/images/site.webmanifest" />
-      </Head>
       <Background>
         <div className="flex-container title-container webpage-title-container">
           <h3>Cor Jesu College of Digos City</h3>
