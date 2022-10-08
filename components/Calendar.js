0a77e7614cd8783145495b46f07b246270edea06
Edmund Nillas III <munilla333d@gmail.com>
2022-10-08 17:03:15 +0800

    Minor refactor of html class name from content to container

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,6 +1,6 @@
 export default function Calendar({ month, children }) {
   return (
-    <div className="flex-container calendar">
+    <div className="flex-container calendar-container">
       <h3>Month of {month}</h3>
       <div className="flex-container dates">{children}</div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className="home">
       <Background>
-        <div className="flex-container title-container head-main-content">
+        <div className="flex-container title-container head-main-container">
           <h3>Cor Jesu College of Digos City</h3>
           <h3>College of Computer and Information Science</h3>
           <p>Home Page</p>
@@ -50,7 +50,7 @@ export default function Home() {
           <div>There are no events for this month</div>
         )}
       </Calendar>
-      <div className="flex-container announcement-content">
+      <div className="flex-container announcement-container">
         <h3>Announcements</h3>
         <p>
           Sed vestibulum mollis est, quis sagittis urna porttitor ac. Donec
