b003e5c63f3e7fa774cae76f4dfd34aa32a52660
Edmund Nillas III <munilla333d@gmail.com>
2022-10-09 08:04:05 +0800

    Revise style of announcement posts

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,11 @@
 export default function Post({ title, info, url }) {
   return (
-    <li className="post">
-      <a href={url}>
-        {title}
+    <li className="flex-container post">
+      <div className="flex-container post-info-container">
+        <h3>{title}</h3>
         <p>{info}</p>
+      </div>
+      <a className="post-link" href={url}>
         <p>See Announcement &gt;&gt;</p>
       </a>
     </li>
